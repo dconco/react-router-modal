989bb115effc2b8ebe385da5596c855739741e45
Dave Conco <118613296+dconco@users.noreply.github.com>
2023-11-27 22:28:37 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import Router from './Router'
 
 import './index.css'
-import './components/styles/tailwind.css'
 
 const Root = createRoot(document.getElementById('root'))
-Root.render(<div className="bg-blue-500 text-white p-4">
-    This should have a blue background with white text.
-</div>)
+Root.render(
+    <React.StrictMode>
+        <Router />
+    </React.StrictMode>
+)
